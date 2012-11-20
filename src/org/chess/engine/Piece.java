8cd92cf37d11088f38bf4933c8fd0b856d0217c1
Peter Hall <pwhall@email.arizona.edu>
2012-11-20 14:23:28 -0700

    intermediate commit, still making these classes

--- src/org/chess/engine/Piece.java
+++ src/org/chess/engine/Piece.java
@@ -1,7 +1,15 @@
 package org.chess.engine;
 
-public class Piece
+public abstract class Piece
 {
 	private Square location;
-	//etc, just trying out github commit.
+	
+	public Piece(Square sq)
+	{
+		location = sq;
+	}
+	
+	public abstract boolean isValidMove(Square sq);
+	
+	public abstract void movePiece(Square sq);
 }
--- src/org/chess/engine/Square.java
+++ src/org/chess/engine/Square.java
@@ -2,7 +2,51 @@ package org.chess.engine;
 
 public class Square
 {
-	//member fields
-	//methods
-	//just trying out github...actually code later.
+	private Piece piece;
+	private int[] location;
+	private int rank;			//rows on the chess board, 1 to 8
+	private char file;			//columns on the chess board, a to h
+	
+	/*
+	 * Note that 1a is a black square in lower left corner, with white player on bottom of board
+	 * and 8h is a black square in upper right.
+	 * 
+	 * Let's use the convention that our integer index location is (1,1) to (8,8) with (1,1) in lower
+	 * left corner, and (8,8) in upper right corner.  That way we only need to map the file, as rank
+	 * will already be the same number.  The rank and file will be important if we want the game
+	 * to output proper chess game play when the game is recorded.
+	 */
+	
+	public Square(int x, int y)
+	{
+		location = new int[2];
+		location[0] = x;
+		location[1] = y;
+		
+		rank = y;
+		switch (x)
+		{
+        	case 1: 	file = 'a';
+                 		break;
+        	case 2: 	file = 'b';
+                 		break;
+            case 3: 	file = 'c';
+                 		break;
+            case 4: 	file = 'd';
+                 		break;
+            case 5: 	file = 'e';
+         				break;
+            case 6: 	file = 'f';
+            			break;
+            case 7: 	file = 'g';
+         				break;
+            case 8: 	file = 'h';
+         				break;
+            default: 	file = 'x';
+                 		break;
+		}
+		
+		piece = null;			// piece not instantiated here, added by
+								// pointing to a Piece in a method below
+	}
 }
