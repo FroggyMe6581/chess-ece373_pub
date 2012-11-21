2f56bab82f66e949dedc29f12c08960409e07e1c
Evan Briones <erbriones@gmail.com>
2012-11-20 18:07:15 -0700

    Cleaned up and removed duplicate code.

--- src/org/chess/engine/Piece.java
+++ src/org/chess/engine/Piece.java
@@ -1,25 +1,21 @@
 package org.chess.engine;
 
-public abstract class Piece
-{
+public abstract class Piece {
 	private Square location;
-	
-	public Piece(Square sq)
-	{
+
+	public Piece(Square sq) {
 		location = sq;
 	}
-	
-	public Square getLocation()
-	{
+
+	public Square getLocation() {
 		return location;
 	}
-	
-	public void setLocation(Square sq)
-	{
+
+	public void setLocation(Square sq) {
 		location = sq;
 	}
-	
+
 	public abstract boolean isValidMove(Square sq);
-	
+
 	public abstract void movePiece(Square sq);
 }
--- src/org/chess/engine/Rook.java
+++ src/org/chess/engine/Rook.java
@@ -1,43 +1,37 @@
 package org.chess.engine;
 
-public class Rook extends Piece
-{
-	
+public class Rook extends Piece {
+
 	private String name = "Rook";
 
-	public Rook(Square sq)
-	{
+	public Rook(Square sq) {
 		super(sq);
 	}
 
-	public String getPieceType()
-	{
+	public String getPieceType() {
 		return name;
 	}
-	
+
 	public boolean isValidMove(Square sq)
-	//Square sq is potential destination square for piece
+	// Square sq is potential destination square for piece
 	{
-		if( ( sq.getLocation()[0] == this.getLocation().getLocation()[0] && 
-			  sq.getLocation()[1] != this.getLocation().getLocation()[1]    ) ||
-			( sq.getLocation()[1] == this.getLocation().getLocation()[1] &&
-			  sq.getLocation()[0] != this.getLocation().getLocation()[0]    )   )
-		{
+		if ((sq.getLocation()[0] == this.getLocation().getLocation()[0] && sq
+				.getLocation()[1] != this.getLocation().getLocation()[1])
+				|| (sq.getLocation()[1] == this.getLocation().getLocation()[1] && sq
+						.getLocation()[0] != this.getLocation().getLocation()[0])) {
 			return true;
-		}
-		else
+		} else
 			return false;
 	}
 
 	public void movePiece(Square sq)
-	//Square sq is destination square; move there if legal
+	// Square sq is destination square; move there if legal
 	{
-		if(isValidMove(sq))
-		{
+		if (isValidMove(sq)) {
 			this.getLocation().removePiece();
 			sq.setPiece(this);
 			this.setLocation(sq);
 		}
 	}
-	
+
 }
